3548104f98d5624effeccd95d7b4c4eab6d673fa
Gunnar Gabrielson <Gunnar Gabrielson>
2017-02-18 10:26:34 -0700

    smurfs!

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,14 @@
 angular.module('angularPractice')
 
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl', function($scope, mainService){
+
+
+    mainService.getSmurfs()
+        .then(function(response){
+            $scope.smurfList = response.data;
+            console.log($scope.smurfList);
+        });
+        
 
 
 });
--- js/mainService.js
+++ js/mainService.js
@@ -1,6 +1,29 @@
 angular.module('angularPractice')
 
-.service('mainService', function($http){
+.service('mainService', function($http, $q){
 
+
+    this.getSmurfs = function(){
+        return $http({
+            method: 'GET',
+            url: 'http://smurfs.devmounta.in/smurfs/?format=json'
+        });
+    }
+
+
+    this.getFirstSmurf = function(){
+
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'http://smurfs.devmounta.in/smurfs/?format=json'
+        })
+        .then(function(response){
+            var firstSmurf = response.data.shift();
+            deferred.resolve(firstSmurf);
+        });
+        return deferred.promise;
+
+    }
 
 });
